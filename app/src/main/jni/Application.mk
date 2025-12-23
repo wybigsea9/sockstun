d56f3baee34a5199c27486eccc59fc5e7e8cafc4
wybigsea <34116919+wybigsea9@users.noreply.github.com>
2025-12-23 21:18:53 +0800

    Update Application.mk

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -16,6 +16,6 @@
 APP_OPTIM := release
 APP_PLATFORM := android-29
 APP_ABI := armeabi-v7a arm64-v8a
-APP_CFLAGS := -O3 -DPKGNAME=hev/sockstun
+APP_CFLAGS := -O3 -DPKGNAME=com/xuanji/agent/spidernode
 APP_CPPFLAGS := -O3 -std=c++11
 NDK_TOOLCHAIN_VERSION := clang
